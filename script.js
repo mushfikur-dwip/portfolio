11021e953bfb36e9ef5c52e5cc70273845dfa792
mushfikur-dwip <musfikurrahmandip@gmail.com>
2024-01-23 23:12:19 +0600

    My Social Connects

---
+++ script.js
@@ -0,0 +1,106 @@
+    class TextScramble {
+  constructor(el) {
+    this.el = el
+    this.chars = '!<>-_\\/[]{}—=+*^?#________'
+    this.update = this.update.bind(this)
+  }
+  setText(newText) {
+    const oldText = this.el.innerText
+    const length = Math.max(oldText.length, newText.length)
+    const promise = new Promise((resolve) => this.resolve = resolve)
+    this.queue = []
+    for (let i = 0; i < length; i++) {
+      const from = oldText[i] || ''
+      const to = newText[i] || ''
+      const start = Math.floor(Math.random() * 50)
+      const end = start + Math.floor(Math.random() * 50)
+      this.queue.push({ from, to, start, end })
+    }
+    cancelAnimationFrame(this.frameRequest)
+    this.frame = 0
+    this.update()
+    return promise
+  }
+  update() {
+    let output = ''
+    let complete = 0
+    for (let i = 0, n = this.queue.length; i < n; i++) {
+      let { from, to, start, end, char } = this.queue[i]
+      if (this.frame >= end) {
+        complete++
+        output += to
+      } else if (this.frame >= start) {
+        if (!char || Math.random() < 0.28) {
+          char = this.randomChar()
+          this.queue[i].char = char
+        }
+        
+        output += `<span class="dud">${char}</span>`
+      } else {
+        output += from
+      }
+    }
+    this.el.innerHTML = output
+    if (complete === this.queue.length) {
+      this.resolve()
+    } else {
+      this.frameRequest = requestAnimationFrame(this.update)
+      this.frame++
+    }
+  }
+  randomChar() {
+    return this.chars[Math.floor(Math.random() * this.chars.length)]
+  }
+}
+
+
+const phrases = [
+  'Hello!',
+  "Cheers for being here!",
+  'How was going?',
+  'To connect with me',
+  'Facebook',
+  'fb.me/mushfikur.a.k',
+  'Facebook_Page',
+  'fb.me/mushfikurdwip',
+  'Messenger',
+  'me.me/mushfikur.a.k',
+  'LinkedIn',
+  'in/mushfikurdwip',
+  'GitHub',
+  '/mushfikur-dwip',
+  'WhatsApp',
+  '01303252317',
+  'X',
+  '@mushfikur__dwip',
+  'Instragram',
+  '/mushfikur_dwip',
+  'Threads',
+  '@mushfikur_dwip',
+  'Discord',
+  '/mushfikurdwip', 
+  'Youtube',
+  '@mushfikur_dwip',
+  'Telegram',
+  '01903812566',
+  'Personal',
+  '01903812566',
+  "Yeah! That's me", 
+  'Have A Good Day', 
+  'See you again!',
+  
+]
+
+const el = document.querySelector('.text')
+const fx = new TextScramble(el)
+
+let counter = 0
+const next = () => {
+  fx.setText(phrases[counter]).then(() => {
+    setTimeout(next, 1000)
+  })
+  counter = (counter + 1) % phrases.length  
+}
+
+next()
+
